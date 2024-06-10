0322cc60fda9c3ca3dcf5c9304c76264ffd2ad0b
Emmiiez <emmiiez.m@gmail.com>
2024-06-10 13:34:42 +0700

    Update ex-3.js
    Update ex-3.js

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,19 @@
-// Start coding here
+// 1
+let add = (a, b) => a+b;
+let result = add (10, 5);
+console.log(result);
+
+// 2
+let subtract =(a, b) => a-b;
+let result = subtract (10, 5);
+console.log(result);
+
+// 3
+let multiply = (a, b) => a*b;
+let result = multiply (10, 5);
+console.log(result);
+
+// 4
+let divide = (a, b) => a/b;
+let result = divide (10, 5);
+console.log(result);// Start coding here
