5ec91fb3bdef71db3fab650a09c4c89b857665cf
Emmiiez <emmiiez.m@gmail.com>
2024-06-10 14:37:24 +0700

    Update ex-4.js
    Update ex-4.js

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,11 @@
-// Start coding here
 let calculator = {};
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+let addResult = add(10, 20);
+console.log(addResult);
+
+let divideResult = divide (3000, 10);
+console.log(divideResult);
